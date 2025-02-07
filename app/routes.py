4c471e46878869085321e637b6b0dde677618098
Juliandos <95juliandos@gmail.com>
2025-02-07 13:13:48 -0600

    ruta para la vista de gestión de vehículos

--- app/routes.py
+++ app/routes.py
@@ -9,3 +9,7 @@ info_template = {
 @app.route('/')
 def index():
     return render_template('index.html', info_template=info_template)
+
+@app.route('/vehiculo_tipo')
+def vehiculo_tipo():
+    return render_template('vehiculo_tipo.html', titulo='Tipo de Vehiculo')
