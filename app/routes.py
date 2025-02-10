9b90938010749e594589006d99b757b404699960
Juliandos <95juliandos@gmail.com>
2025-02-10 13:02:48 -0600

    ruta para eliminar un vehiculoTipo

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, render_template
 from app.models import VehiculoTipo, TarifaTipo, Tarifa, Modulo, Vehiculo, Parqueo, Punto, Redimir, Arrendamiento, Sede, Pais, Usuario, Rol, Periodicidad, Cliente, MedioPago, Parqueadero
+from app import db
 
 routes = Blueprint('routes', __name__)
 
@@ -12,7 +13,16 @@ info_template = {
 def index():
     return render_template('index.html', info_template=info_template)
 
+# VehiculoTipo all
 @routes.route('/vehiculo_tipo')
 def vehiculo_tipo():
     tipos_vehiculo = VehiculoTipo.query.all()
     return render_template('vehiculo_tipo.html', titulo='Tipo de Vehiculo', tipos_vehiculo = tipos_vehiculo)
+
+# VehiculoTipo DELETE
+
+@routes.route('/vehiculo_tipo/delete/<int:id>', methods=['POST'])
+def vehiculo_tipo_delete(id):
+    tipo_vehiculo = VehiculoTipo.query.get_or_404(id)
+    db.session.delete(tipo_vehiculo)
+    db.session.commit()
